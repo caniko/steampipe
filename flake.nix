4a8ccc04926d1919ae0ba85e1e6d6ce4432a157c
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-03-16 22:38:47 +0100

    Add flake.nix for Nix-based builds
    Uses rustPlatform.buildRustPackage with nightly toolchain via rust-overlay.

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "NixOS microVM cluster orchestrator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+
+      toolchain = pkgs.rust-bin.nightly.latest.minimal;
+
+      cluster-ctl = pkgs.rustPlatform.buildRustPackage {
+        pname = "cluster-ctl";
+        version = "0.1.0";
+        src = ./.;
+        useFetchCargoVendor = true;
+        cargoHash = "sha256-+zUfZHaWBUK32tg+eAZp73oAPQVFeuFTTUN+n8/o7oM=";
+        nativeBuildInputs = [toolchain];
+      };
+    in {
+      packages.default = cluster-ctl;
+    });
+}
