78fa118745fe34aad41e3c0287f7b8d84c36391d
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-05-07 06:51:20 +0200

    crosvm: enable gfxstream in overlay

--- flake.nix
+++ flake.nix
@@ -193,6 +193,13 @@
           # microvm runner skips that workaround and uses our pre-compiled
           # `.bpf` policies instead.
           version = "107.1-unstable-2026-02-13";
+          buildInputs = (oldAttrs.buildInputs or []) ++ [
+            final.aemu
+            final.gfxstream
+          ];
+          buildFeatures = (oldAttrs.buildFeatures or []) ++ [
+            "gfxstream"
+          ];
           nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
             final.python3
           ];
