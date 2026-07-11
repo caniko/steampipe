f3aa5bc7dca01dc3cd1ab161e3a95fb0595fb2cc
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-11 12:34:50 +0200

    Refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git";
+    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk";
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay = {
       follows = "rs-harbor/rust-overlay";
