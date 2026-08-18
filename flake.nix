cfd6c703f3c62d519481bdfaff30da854f1983ab
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-18 10:53:00 +0200

    chore: bump rs-harbor pin to github mirror f209ddb
    The codefloe/codeberg nix-opencode-lsp URLs died; the pins now point at
    the github rs-harbor rev carrying the github mirror input. Refreshes the
    root and tests/fixture locks.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    rs-harbor.url = "git+ssh://git@codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay = {
       follows = "rs-harbor/rust-overlay";
--- tests/fixture/flake.nix
+++ tests/fixture/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rs-harbor = {
-      url = "git+ssh://git@codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+      url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     steampipe.url = "path:../..";
