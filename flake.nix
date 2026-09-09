13dc2e58a583c9d9af7fae7e691c253a00bd8beb
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-09 20:17:26 +0200

    chore: pin harbor-rs to fac8049

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    harbor-rs.url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
+    harbor-rs.url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=fac8049316846e0ef1c1e6acd92aed7a337b333a";
     rs-harbor.follows = "harbor-rs";
     nixpkgs.follows = "harbor-rs/nixpkgs";
     rust-overlay = {
